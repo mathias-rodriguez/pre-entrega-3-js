04cd1127b19b228a33461774f702a765d8dbd247
mathias rodriguez <rdz.mathi@gmail.com>
2023-09-12 23:20:48 -0300

    añadimos json y footer carrito

--- js/carrito.js
+++ js/carrito.js
@@ -35,19 +35,48 @@ export const comprarProducto = (idProducto) => {
     carrito = JSON.parse(sessionStorage.getItem("carrito"));
 };
 
-// const pintarCarrito = () => {
-//     carrito.forEach(producto => {
-//         let body = document.createElement("tr")
-//         body.className = "producto-carrito"
-//         body.innerHTML = `
-//         <th><img id = "fotoProducto" src = "${producto.img}"></th>
-//         <td>${producto.artista}</td>
-//         <td>${producto.album}</td>
-//         <td>${producto.precio/cantidad}</td>
-//         <td>${precio}</td>
-//         <td>
-//         <button id = "">
-//         </td>
-//         `
-//     });
-// };
+const pintarCarrito = () => {
+    carrito.forEach(producto => {
+        let body = document.createElement("tr")
+        body.className = "producto-carrito"
+        body.innerHTML = `
+        <th><img id = "fotoProducto" src = "${producto.img}"></th>
+        <td>${producto.artista}</td>
+        <td>${producto.album}</td>
+        <td>${producto.precio / cantidad}</td>
+        <td>${precio}</td>
+        <td>
+        <button id = "+${producto.id}"> + </button>
+        <button id = "+${producto.id}"> - </button>
+        </td>
+        `
+        listaCarrito.append(body)
+        const btnAgregar = document.getElementById(` +${id}`)
+        const btnRestar = document.getElementById(` -${id}`)
+
+        btnAgregar.addEventListener("click", () => console.log(id))
+        btnRestar.addEventListener("click", () => console.log(id))
+    });
+    pintarFooter();
+};
+
+const pintarFooter = () => {
+    if (carrito.length > 0) {
+        footCarrito.innerHTML = ""
+        let footer = document.createElement("tr")
+        footer.innerHTML = `
+        <th>Totales:</th>
+        <td></td>
+        <td>Cantidad total</td>
+        <td></td>
+        <td>Costo total</td>
+        `
+        footCarrito.append(footer)
+    }
+    else {
+        footCarrito.innerHTML = "<h3>No hay productos en el carrito</h3>
+    }
+}
+const generarTotales = () => {
+    const costoTotal = carrito.reduce((total, { precio }) => total + precio, 0)
+}
